d0dac2b0fb2652d9fd9336a63fd55365c5d8e9f0
Polina Tiureva <summiteofglory42@gmail.com>
2020-11-13 22:40:27 +0300

    fixed some errors

--- solution.py
+++ solution.py
@@ -9,15 +9,9 @@ class FileReader:
         
     def read(self):
         try:
-            if not os.path.exists(self.path):
-                raise FileNotFoundError
+            return open(self.path).read()
         except FileNotFoundError:
-            content = ''
-        else:
-            file = open(self.path, 'r') 
-            content = file.read()
-            file.close()
-        return content 
+           return ''
 
     def write(self, message):
         with open(self.path, 'a') as file:
@@ -30,18 +24,22 @@ class FileReader:
     
     def count(self):
         tokens = word_tokenize(self.read())
-        setattr(self, 'word_count', len(tokens))
-        lines = len(open(self.path).readlines())
-        setattr(self, 'line_count', lines)
-        return self.word_count, self.line_count
+        self.word_count = len(tokens)
+        # setattr(self, 'word_count', len(tokens))
+        # setattr(self, 'line_count', len(open(self.path).readlines()))
+        try:
+            self.line_count = len(open(self.path).readlines())
+        except FileNotFoundError:
+            self.line_count = 0
+            
+        return f'Word count: {self.word_count}. Line count: {self.line_count}'
     
     def __add__(self, obj):
-        first_basename = os.path.basename(obj.path)
-        second_basename = os.path.basename(self.path)
+        first_basename = os.path.basename(self.path)
+        second_basename = os.path.basename(obj.path)
         name_new_file = f'{os.path.splitext(first_basename)[0]}+{os.path.splitext(second_basename)[0]}.txt' 
         new_obj = FileReader(name_new_file)
-        result_of_add = self.read() + obj.read()
-        new_obj.write(result_of_add)
+        new_obj.write(self.read() + obj.read())
         return new_obj
 
 
@@ -49,7 +47,8 @@ def main():
     reader = FileReader('new.txt')
     reader.write('hello\nworld')
     print(reader.read())  # 'hello\nworld'
-    print(reader.count())  # (2, 2) 
+    print(reader.count()) # (2, 2) 
+    print(reader.word_count)
     new_reader = FileReader('very_new.txt')
     new_reader.write('\nthe most beutiful world')
     print(type(new_reader))  # <class '__main__.FileReader'>
