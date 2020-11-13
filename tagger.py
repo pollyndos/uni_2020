853f288c4033f70139d30b76ce1efca6b342e7cb
Polina Tiureva <summiteofglory42@gmail.com>
2020-11-14 02:09:40 +0300

    fixed tagger.py

--- tagger.py
+++ tagger.py
@@ -1,39 +1,32 @@
-"""
-from corus import load_corpora
-import tqdm
-
-path = 'annot.opcorpora.xml.byfile.zip'
-records = load_corpora(path)
-
-with open('pos_data.txt', 'w', encoding='utf8') as f:
-    for rec in tqdm.tqdm(records):
-        for par in rec.pars:
-            for sent in par.sents:
-                for token in sent.tokens:
-                    f.write(f'{token.text} {token.forms[0].grams[0]}\n')
-print('Done')
-"""
 import pickle
 from sklearn.model_selection import train_test_split
-
-# не так поняла суть метода predict, переделываю
 
 class UnigramMorphAnalyzer:
     
     def __init__(self, path_annot_corpus):
         self.path_annot_corpus = path_annot_corpus
         self.endings_stat = {}
-
-                
+        with open(self.path_annot_corpus, 'r', encoding='utf8') as f:
+            corp = f.readlines()
+            self.train_data, self.test_data = train_test_split(corp, test_size=0.2)
+            f.close()
+            
+    def save(self, name_pickle):
+        with open(name_pickle, 'wb') as f:
+            pickle.dump(self.endings_stat, f)
+    
+    def load(self, name_pickle):
+        with open(name_pickle, 'rb') as f:
+            pickle.load(f)
+   
     def __getitem__(self, ending):
         return self.endings_stat[ending]
     
     def train(self):
-        print('Running train ')
-        all_tags = set()
-        with open(self.path_annot_corpus, 'r', encoding='utf8') as f:
-            for line in f:
-                word, pos = line.split()
+        for line in self.train_data:
+            line = line[:-1]
+            word, pos = line.split()
+            if pos not in ['HANI', 'PUNCT', 'DATE', 'SYMB', 'GREK', 'LATN', 'NUMB']:
                 for i in range(1, 5):
                     ending = word[-i:]
                     if len(ending) < i:
@@ -44,38 +37,56 @@ class UnigramMorphAnalyzer:
                         self.endings_stat[ending][pos] = 1
                     else:
                         self.endings_stat[ending][pos] += 1
-            f.close()
-            return len(self.endings_stat)
-        
-    def predict(self, token):
-        print('Running predict')
-        if token in self.endings_stat:
-            all_counts = sum(self.endings_stat[token].values())
-            res_prob = ''
-            for key in self.endings_stat[token].keys():
-                prob = round(self.endings_stat[token][key] / all_counts, 4)
-                a = f'{key} : {prob}\n'
-                res_prob += a
-            return f'{token} \n{res_prob}'
-        
-    def save(self, name_pickle):
-        print('Running save')
-        with open(name_pickle, 'wb') as f:
-            pickle.dump(self.endings_stat, f)
+        return len(self.endings_stat)
     
-    def load(self, name_pickle):
-        print('Running load')
-        with open(name_pickle, 'rb') as f:
-            pickle.load(f)
+    def predict(self, token):
+        prob_dict = {}
+        all_counts = 0
+        for i in range(1, 5):
+            tok_end = token[-i:]
+            if tok_end in self.endings_stat:
+                all_counts += sum(self.endings_stat[tok_end].values())
+                if len(tok_end) < i:
+                    break
+                for key in self.endings_stat[tok_end].keys():
+                    if key not in prob_dict:
+                        prob_dict[key] = self.endings_stat[tok_end][key]
+                    else:
+                        prob_dict[key] += self.endings_stat[tok_end][key]
+        
+        for key, val in prob_dict.items():
+            prob_dict[key] = round(val/all_counts, 4)
             
-           
-uni = UnigramMorphAnalyzer('pos_data.txt')
-z = uni.train()
-print(z)
-print(uni)
-print(uni.predict('ова'))
-print(uni['ий'])
-uni.save('model.pickle')
-uni.load('model.pickle')
-
-
+        return prob_dict
+    
+    def eval(self):
+        correct_pos = 0
+        all_pos = 0
+        
+        for line in self.test_data:
+            line = line[:-1]
+            word, pos = line.split()
+            if pos not in ['HANI', 'PUNCT', 'DATE', 'SYMB', 'GREK', 'LATN', 'NUMB']:
+                try:
+                    predicted_pos = max(self.predict(word), key=self.predict(word).get)
+                except ValueError:
+                    # print(word, pos) # странные символы
+                    all_pos -= 1
+                if pos == predicted_pos:
+                    correct_pos += 1
+                all_pos += 1
+        return round(correct_pos / all_pos, 4)
+        
+def main():
+    uni = UnigramMorphAnalyzer('pos_data.txt')
+    z = uni.train()
+    print(z)
+    print(uni)
+    print(uni.predict('голова'))
+    print(uni['ий'])
+    uni.save('model.pickle')
+    uni.load('model.pickle')
+    print(uni.eval())
+    
+if __name__ == '__main__':
+    main()
